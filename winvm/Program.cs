418bea64bf8e03e1f6b067e94189001d1b643e4f
desktopgame <gnndr36@gmail.com>
2019-07-24 14:07:01 +0900

    refactor

--- winvm/Program.cs
+++ winvm/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Microsoft.Win32;
 using Microsoft.Extensions.CommandLineUtils;
+using System.Text;
 
 namespace winvm
 {
-    class Program {
+    class Program
+    {
         static readonly string ROOT = "SYSTEM\\CurrentControlSet\\Enum";
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             var app = new CommandLineApplication(throwOnUnexpectedArg: false);
 
             app.Name = nameof(Program);
@@ -20,26 +24,31 @@ namespace winvm
                 optionType: CommandOptionType.SingleValue
             );
 
-            app.OnExecute(() => {
+            app.OnExecute(() =>
+            {
                 app.ShowHelp();
                 return 1;
             });
             //saveコマンド
-            app.Command("save", (command) => {
+            app.Command("save", (command) =>
+            {
                 command.Description = "レジストリの中身を保存する";
                 command.HelpOption("-?|-h|--help");
                 var outputArgs = command.Argument("output", "保存先のパス");
-                command.OnExecute(() => {
-                    File.WriteAllText(outputArgs.Value, OpenRegRecursive(GetRegistryPath(pathOption),0));
+                command.OnExecute(() =>
+                {
+                    File.WriteAllText(outputArgs.Value, GetRegTextRecursive(GetRegistryPath(pathOption)));
                     return 0;
                 });
             });
             //loadコマンド
-            app.Command("load", (command) => {
+            app.Command("load", (command) =>
+            {
                 command.Description = "レジストリの中身を復元する";
                 command.HelpOption("-?|-h|--help");
                 var outputArgs = command.Argument("input", "読み込み元のパス");
-                command.OnExecute(() => {
+                command.OnExecute(() =>
+                {
                     var location = outputArgs.Value;
                     Console.WriteLine("Hoge: " + location);
                     return 0;
@@ -48,29 +57,57 @@ namespace winvm
             app.Execute(args);
         }
 
-        static string GetRegistryPath(CommandOption option) {
-            if(option.HasValue()) {
+        static string GetRegistryPath(CommandOption option)
+        {
+            if (option.HasValue())
+            {
                 return option.Value();
             }
             return ROOT;
         }
-        
-        static string OpenRegRecursive(string key, int depth) {
-            var sb = new System.Text.StringBuilder();
-            sb.AppendLine(key);
-            try {
+
+        static string GetRegTextRecursive(string key)
+        {
+            var e = GetRegRecursive(key);
+            var buf = new StringBuilder();
+            buf.AppendLine(e.FullPath());
+            GetRegTextRecursive(e, buf);
+            return buf.ToString();
+        }
+
+        static void GetRegTextRecursive(RegEntry e, StringBuilder sb)
+        {
+            for(int i=0; i<e.Count; i++)
+            {
+                sb.AppendLine(e[i].FullPath());
+                GetRegTextRecursive(e[i], sb);
+            }
+        }
+
+        static RegEntry GetRegRecursive(string key)
+        {
+            return GetRegRecursive(key, key);
+        }
+
+        static RegEntry GetRegRecursive(string key, string name)
+        {
+            try
+            {
+                var entry = new RegEntry(name, "v");
                 RegistryKey rParentKey =
                      Registry.LocalMachine.OpenSubKey(key);
                 string[] arySubKeyNames = rParentKey.GetSubKeyNames();
                 rParentKey.Close();
                 foreach (string subKeyName in arySubKeyNames)
                 {
-                    sb.Append(OpenRegRecursive(key + "\\" + subKeyName, depth + 1));
+                    entry.Add(GetRegRecursive(key + "\\" + subKeyName, subKeyName));
                 }
-            } catch(System.Security.SecurityException se) {
-                sb.AppendLine("ocurred exception: " + key);
+                return entry;
             }
-            return sb.ToString();
+            catch (System.Security.SecurityException se)
+            {
+                return (RegEntry.Error(key));
+            }
         }
     }
 }
---
+++ winvm/RegEntry.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace winvm
+{
+    class RegEntry
+    {
+        public string Key { private set; get; }
+        public object Value { private set; get; }
+        public bool IsError { private set; get; }
+        public int Count {
+            get { return children.Count; }
+        }
+        public  RegEntry this[int i] {
+            get {
+                return children[i];
+            }
+        }
+
+        private RegEntry parent;
+        private List<RegEntry> children;
+
+        public RegEntry(string key, object value)
+        {
+            this.Key = key;
+            this.Value = value;
+            this.children = new List<RegEntry>();
+        }
+        public static RegEntry Error(string key)
+        {
+            var ret = new RegEntry(key, "");
+            ret.IsError = true;
+            return ret;
+        }
+        public void Add(RegEntry e)
+        {
+            e.parent = this;
+            children.Add(e);
+        }
+
+        public string FullPath()
+        {
+            if(parent == null)
+            {
+                return Key;
+            }
+            return parent.FullPath(new RegEntry[] { this});
+        }
+
+        private string FullPath(RegEntry[] children)
+        {
+            if (parent == null)
+            {
+                var sb = new StringBuilder();
+                sb.Append(Key).Append("\\");
+                foreach(var a  in children)
+                {
+                    sb.Append(a.Key).Append("\\");
+                }
+                sb.Remove(sb.Length - 1, 1);
+                return sb.ToString();
+            } else
+            {
+                var li = new List<RegEntry>();
+                li.AddRange(children);
+                li.Add(this);
+                return parent.FullPath(li.ToArray());
+            }
+        }
+    }
+}

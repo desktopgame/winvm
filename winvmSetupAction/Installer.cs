320b4da779743a6d7e1a003a5046fb5f275ebcbf
desktopgame <gnndr36@gmail.com>
2019-07-25 12:59:39 +0900

    temp

--- winvmSetupAction/Installer.cs
+++ winvmSetupAction/Installer.cs
@@ -8,41 +8,33 @@ using System.IO;
 namespace winvmSetupAction
 {
     [System.ComponentModel.RunInstaller(true)]
-    class Installer : System.Configuration.Install.Installer
+    public class Installer : System.Configuration.Install.Installer
     {
 
-        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+
         public override void Install(System.Collections.IDictionary stateSaver)
         {
             base.Install(stateSaver);
-            var addToPath = bool.Parse(this.Context.Parameters["addToPath"]);
-            if(addToPath)
-            {
-                File.WriteAllText("install.text", "true");
-            } else
-            {
-                File.WriteAllText("install.text", "false");
-            }
-            System.Windows.Forms.MessageBox.Show("テスト");
+
+            var addToPath = (this.Context.Parameters["addToPath"]);
+            System.Windows.Forms.MessageBox.Show(addToPath.ToString());
         }
 
-        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
         public override void Commit(System.Collections.IDictionary savedState)
         {
             base.Commit(savedState);
         }
 
-        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
         public override void Rollback(System.Collections.IDictionary savedState)
         {
             base.Rollback(savedState);
         }
 
-        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
         public override void Uninstall(System.Collections.IDictionary savedState)
         {
             base.Uninstall(savedState);
             File.Delete("install.text");
+            System.Windows.Forms.MessageBox.Show("テスト2");
         }
 
         private void InitializeComponent()
